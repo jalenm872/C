0b46ced914da54fe25e0da2fa45dea58f8c17564
Jalen Mendoza <jalen_mendoza@my.uri.edu>
2020-10-16 17:09:55 -0400

    1 done

--- pa2/draw_triangle_1.cpp
+++ pa2/draw_triangle_1.cpp
@@ -2,18 +2,22 @@
 //Draw left-justified traingel 0<rows<20
 
 //Function
-int triangle(){
+int main(){
     //Variable
-    int row, i , j;
+    int row;
     //User input
-    std::cout << "Enter Rows\n";
     std::cin >> row;
     //If and nested for loops
-    for(i; i <= row; i++){
-        for(j; j <= i; j++){
-            std::cout << "+ ";
+    if(row > 0 && row < 20){
+        for(int i = 1; i <= row; i++){
+            for(int j = 1; j <= i; j++){
+                std::cout << "+ ";
         }
-        std::cout << "\n";
+        std::cout << std::endl;
     }
     return 0;
+    }
+    else{
+        std::cout << "Error\n";
+    }
 }
