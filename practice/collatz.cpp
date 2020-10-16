1f159220227bc650cfbe4e25d864ee8cbda21813
Jalen Mendoza <jalen_mendoza@my.uri.edu>
2020-10-16 12:57:39 -0400

    practice

---
+++ practice/collatz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+float collatz(int n){
+    int steps = 0;
+    while (n != 1){
+        if ( n % 2 == 0){
+            n = n /2;
+        }
+        else{
+            n = n*3 +1;
+        }
+        steps ++;
+    }
+}
---
+++ practice/even_sum.cpp
@@ -0,0 +1,10 @@
+//Even Sum
+
+int main(){
+    int i;
+    std::cout << int n;
+    while (i <= n){
+        multiples
+        if ()
+    }
+}
