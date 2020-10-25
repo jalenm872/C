bfcd2494aa41b4dd13810e556b8154a3bcc2db7f
Jalen Mendoza <jalen_mendoza@my.uri.edu>
2020-10-25 18:37:01 -0400

    pa2

--- pa2/main_10.cpp
+++ pa2/main_10.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
 int main(){
-    float loan,interest,payment, month;
-    month = 0;
+    double loan, interest, payment;
+    double month = 0;
     std::cin >> loan >> interest >> payment;
-    for(float i = 0; i < loan; i++){
+    for(int i = 0; i < loan; i++){
         loan = loan + loan * (1 + interest);
         //Loan amount after every month paid
         loan = loan - payment;
-        month++;
     }
     std::cout << month << std::endl;
     return 0;
