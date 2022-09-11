21045b93768a99958c555a89b8808e315b8d767b
Jalen Mendoza <jalen_mendoza@my.uri.edu>
2022-09-11 17:22:26 -0400

    Continued my review of OOP

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,11 +1,17 @@
 //Pracitce with OOP
 
 #include <iostream>
+using namespace std;
 using std::string;
 
+//Abstraction
+class AbstractEmployee{
+    virtual void AskForPromotion() = 0;
+};
+
 //Employee Class
-class Employee{
-    public:
+class Employee:AbstractEmployee{
+    private:
         //Name of employee
         string Name;
 
@@ -15,21 +21,65 @@ class Employee{
         //Age of employee
         int Age;
 
+    public:
+        //Setter Methods
+        void setName(string name){
+            Name = name;
+        }
+
+        void setCompany(string company){
+            Company = company;
+        }
+
+        void setAge(int age){
+            if(age >= 18){
+                Age = age;
+            }
+        }
+
+        //Getter Methods
+        string getName(){
+            return Name;
+        }
+
+        string getCompany(){
+            return Company;
+        }
+
+        int getAge(){
+            return Age;
+        }
+
+        //Method
         void IntroduceYourself(){
-            std::cout << "Name - " << Name << std::endl;
-            std::cout << "Company - " << Company << std::endl;
-            std::cout << "Age - " << Age << std::endl;
+            cout << "Name - " << Name << endl;
+            cout << "Company - " << Company << endl;
+            cout << "Age - " << Age << endl;
+        }
+
+        //Constructor
+        Employee(string name, string company, int age){
+            Name = name;
+            Company = company;
+            Age = age;
+        }
+
+        void AskForPromotion(){
+            if(Age > 30){
+                cout << Name << " got promoted!" << endl;
+            }
+            else{
+                cout << Name << " sorry no promotion for you!" << endl;
+            }
         }
 };
 
 int main()
 {
-    //Create Instance of Class Employee
-    Employee employee1;
-    employee1.Name = "Jalen";
-    employee1.Company = "Meta";
-    employee1.Age = 21;
+    //Create Instances of Class Employee
+    Employee employee1("Jalen", "Meta", 21);
+    Employee employee2("Jeraldyn", "Law Firm", 31);
 
-    //Calling Class Method
-    employee1.IntroduceYourself();
+    employee1.AskForPromotion();
+    employee2.AskForPromotion();
 }
